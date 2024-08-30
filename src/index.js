df668a0a6b91b51a720a06a43bfab563e337dfe8
Favour Ajor <pretty.ajor@gmail.com>
2024-08-30 22:50:10 +0100

    Fix

--- src/index.js
+++ src/index.js
@@ -25,22 +25,24 @@ function updateTime() {
 function updateCity(event) {
   let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
-    cityTimeZone = moment().tz.guess();
+    cityTimeZone = moment.tz.guess();
   }
+  console.log(cityTimeZone);
   let cityName = cityTimeZone.replace("-", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
-  let citiesElement = document.quserySelector("#cities");
-  citiesElement.innerHTML = (
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
     <div class="city">
       <div>
         <h2>${cityName}</h2>
         <div class="date">${cityTime.format("MMMM Do, YYYY")}</div>
       </div>
       <div class="time">
-        ${cityTime.format("h:mm:ss")} <small>${cityTime.forma("A")}</small>
+        ${cityTime.format("h:mm:ss")} <small>${cityTime.format("A")}</small>
       </div>
     </div>
-  );
+    <a href = "/">All Cities</a>
+  `;
 }
 updateTime();
 setInterval(updateTime, 1000);
